7b9a77c183876dbbc55a0a3b12c94b18105c5725
KristoferVildin <kristofer.vildin@gmail.com>
2025-10-26 19:34:06 +0100

    lagt till flera spelare, lagt till header

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,19 @@ function App() {
 
   return (
     players && (
+      <div>
+      <h1>NHL Stats Leaders 2025 Season</h1>
     <ol>
       {players.map((player) => (
         <li key={player.id}>{player.name} - {player.points}p</li>
       ))}
     </ol>
+    <p style={{display: 'flex', justifyContent: 'flex-end', fontWeight: 'bold'}}>
+      2025-26/10</p>
+    </div>
     )
   )
+  
 }
 
 export default App
